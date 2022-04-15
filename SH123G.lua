17a059bacf3079f54fc6c2762bb39a017c3ec999
38759238u592385092348509432857 <85268584+SoSAstronaut@users.noreply.github.com>
2022-04-15 02:23:38 -0600

    Create SH123G.lua

---
+++ SH123G.lua
@@ -0,0 +1,14 @@
+local userInput = game:service('UserInputService')
+			local runService = game:service('RunService')
+			
+			userInput.InputBegan:connect(function(Key)
+				if Key.KeyCode == Enum.KeyCode.G then
+					Enabled = not Enabled
+					if Enabled == true then
+						repeat
+							game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame + game.Players.LocalPlayer.Character.Humanoid.MoveDirection * getgenv().SpeedMultiplier
+							runService.Stepped:wait()
+						until Enabled == false
+					end
+				end
+			end)
